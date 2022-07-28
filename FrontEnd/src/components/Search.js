836ecd9551624f5240ac1a73ec52c0d96d3035f9
AYOUB-NBP <supaboy96@gmail.com>
2022-07-28 04:16:39 +0000

    I wonder what happens when I push this. I started styling the search page and I want to save my progress

--- FrontEnd/src/components/Search.js
+++ FrontEnd/src/components/Search.js
@@ -1,54 +1,44 @@
-import { Input, Menu, Container, Image } from 'semantic-ui-react';
+import { Input, Menu, Image, Icon, Container } from 'semantic-ui-react';
+import HorizontalScroll from 'react-scroll-horizontal';
 import '../styles/search.scss';
-
+const child = {width:'200px',height:'200px'}
 function SearchBar() {
   return (
     <div className='search-container'>
-      <Menu 
-        inverted
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: 'transparent',
-          borderRadius: '0',
-        }}
-      >
-      <Menu.Item
-        style={{
-          width: '700px',
-          height: '50px',
-        }}
-      >
+      <Menu inverted>
+        <Menu.Item>
+          <Icon name='search' />
           <Input
-            icon= 'sliders horizontal'
-            placeholder=' 🔍 Rechercher'
+            className='input-custom'
+            icon='sliders horizontal inverted'
+            placeholder='Rechercher'
             size='large'
           />
-          
         </Menu.Item>
       </Menu>
-      <Container
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: 'white',
-          borderRadius: '20px',
-          width: '700px',
-          height: '400px',
-          marginTop: '10px',
-        }}
-      >
-        <Image
-          src='https://picsum.photos/1080/1920' 
-          style={{
-            objectFit: 'cover',
-            width: '100%',
-            height: '100%',
-            borderRadius: '20px',
-          }}
-        />
+      <Container className='results'>
+        <div className='container-custom'>
+          <Container className='image-container'>
+            <Image
+              className='image-custom'
+              src='https://picsum.photos/1080/1920?random=1'
+            />
+            <div className='image-content'>
+            </div>
+          </Container>
+        </div>
+        <HorizontalScroll style={{height:'200px'}}>
+          <div className="test" style={child}></div>
+          <div className="test" style={child}></div>
+          <div className="test" style={child}></div>
+          <div className="test" style={child}></div>
+          <div className="test" style={child}></div>
+          <div className="test" style={child}></div>
+          <div className="test" style={child}></div>
+          <div className="test" style={child}></div>
+        </HorizontalScroll>
       </Container>
-    </div>
+    </div >
   );
 }
 
