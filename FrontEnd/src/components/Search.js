9955f4a78acd80c43c65bacebc803611e2c0f8a9
AYOUB-NBP <supaboy96@gmail.com>
2022-07-29 23:14:09 +0000

    Pretty cool, good practice

--- FrontEnd/src/components/Search.js
+++ FrontEnd/src/components/Search.js
@@ -3,13 +3,16 @@ import HorizontalScroll from 'react-scroll-horizontal';
 import '../styles/search.scss';
 
 // The top value controls carousel position:May require changing is '.image-custom' height is modified in search.scss
-const HorizontalScrollHeight = { height: '125px', top: '0px' };
-const ElementWidth = { width: '200px', maxWidth: 'none' }
+const ElementWidth = { width: '36vw',height:'100%', maxWidth: 'none' } //carousel images size
+const whiteColor = { color: "white" } //For Catégories >
+const horiScrollWidthHeight = { width: 'calc(100% - 2em', height: '22vw', top: '0px', borderRadius:'15px' }// Carousel Width Correction
 
 function SearchBar() {
   return (
     <div className='search-container'>
+
       <Container>
+
         <Menu inverted>
           <Menu.Item>
             <Icon name='search' />
@@ -21,6 +24,7 @@ function SearchBar() {
             />
           </Menu.Item>
         </Menu>
+
         <Container className='image-container'>
           <Image
             className='image-custom'
@@ -30,43 +34,131 @@ function SearchBar() {
             <div className='wrapper'><div className='date'>
               <div id='cap'>28</div>
               <div id='noCap'>JUN</div>
-              </div>
+            </div>
               <h1>Michael Korsten</h1>
-              <p style={{fontSize:'78%'}}>Le DJ star des nuits New Yorkaises débarque à Paris <br></br> pour une soirée de dingue...</p></div>
+              <p style={{ fontSize: '78%' }}>Le DJ star des nuits New Yorkaises débarque à Paris <br></br> pour une soirée de dingue...</p></div>
           </div>
+          <Icon name='heart outline' size='big' />
+        </Container>
 
+        <Container className='heading' >
+          <h2 style={whiteColor}>Catégories &gt;</h2>
         </Container>
-        <Container style={{margin:'0'}}>
-          <h2 style={{ color: 'white' }}>Catégories &gt;</h2>
-          <br></br>
-        </Container>
-        <HorizontalScroll style={HorizontalScrollHeight}>
-          <Image
-            className='scroll-elements'
-            style={ElementWidth}
-            src='https://picsum.photos/1080/1920?random=2'
-          />
-          <Image
-            className='scroll-elements'
-            style={ElementWidth}
-            src='https://picsum.photos/1080/1920?random=3'
-          />
-          <Image
-            className='scroll-elements'
-            style={ElementWidth}
-            src='https://picsum.photos/1080/1920?random=4'
-          />
-          <Image
-            className='scroll-elements'
-            style={ElementWidth}
-            src='https://picsum.photos/1080/1920?random=5'
-          />
-          <Image
-            className='scroll-elements'
-            style={ElementWidth}
-            src='https://picsum.photos/1080/1920?random=6'
-          />
+
+        <HorizontalScroll style={horiScrollWidthHeight}>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=2'
+            />
+            <div className="labelName">Concert</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=3'
+            />
+            <div className="labelName">Festival</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=4'
+            />
+            <div className="labelName">Cocktail Party</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=5'
+            />
+            <div className="labelName">Fashion Show</div>
+          </div>
         </HorizontalScroll>
+
+        <Container className='heading' >
+          <h2 style={whiteColor}>Top 30 en favoris &gt;</h2>
+        </Container>
+
+        <HorizontalScroll style={horiScrollWidthHeight}>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=10'
+            />
+            <div className="labelName">Concert</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=11'
+            />
+            <div className="labelName">Festival</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=12'
+            />
+            <div className="labelName">Cocktail Party</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=13'
+            />
+            <div className="labelName">Fashion Show</div>
+          </div>
+        </HorizontalScroll>
+
+        <Container className='heading' >
+          <h2 style={whiteColor}>Chef ... J'ai grave la dalle !</h2>
+        </Container>
+
+        <HorizontalScroll style={horiScrollWidthHeight}>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=6'
+            />
+            <div className="labelName">Concert</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=7'
+            />
+            <div className="labelName">Festival</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=8'
+            />
+            <div className="labelName">Cocktail Party</div>
+          </div>
+          <div className='cards'>
+            <Image 
+              className='scroll-images'
+              style={ElementWidth}
+              src='https://picsum.photos/1080/1920?random=9'
+            />
+            <div className="labelName">Fashion Show</div>
+          </div>
+        </HorizontalScroll>
+
+        
       </Container>
     </div >
   );
